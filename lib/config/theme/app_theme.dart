057c1343347eabeaa68e0e8d8e01e2e998d20e0e
JuanBueno21 <juancamilobuenomintic@gmail.com>
2023-10-30 16:12:34 -0500

    [Widgets] - creacion de boton con lista de colores

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+
+const colorList = <Color>[
+  Colors.blue, 
+  Colors.teal,
+  Colors.white,
+  Colors.yellow,
+  Colors.red,
+  Colors.purple,
+  Colors.pink,
+  Colors.orange,
+  Colors.grey,
+];
+
+
+class AppTheme {
+
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }): assert(selectedColor >= 0, "El color seleccionado debe ser mayor que 0"),
+      assert(selectedColor < colorList.length -1, 
+      "El color seleccionado debe ser menor o igual que ${colorList.length}"
+    
+    );
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList [selectedColor],
+  );
+
+}
